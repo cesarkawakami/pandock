6633db354a36d94d36bfa81566e1405f8d7f8215
Cesar Kawakami <cesarkawakami@gmail.com>
2012-12-14 15:14:47 -0200

    Bumping version

--- pandock.user.js
+++ pandock.user.js
@@ -2,7 +2,7 @@
 ==UserScript==
 @name       Pandock: Flowdock UX fixes
 @namespace  cesarkawakami
-@version    0.2
+@version    0.3
 @description  Adds fixes to Flowdock UX, like keyboard shortcuts and a more readable UI
 @match      http*://*.flowdock.com/app/*
 @copyright  2012+, Cesar Kawakami
